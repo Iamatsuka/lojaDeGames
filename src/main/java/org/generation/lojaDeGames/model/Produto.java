81a8b7331ffc5f4bb55395f1a245eed76426a238
vanessa.isabella.oliveira@gmail.com <caiqueiamatsuka@gmail.com>
2023-02-10 17:55:31 -0300

    CRUD loja de games implementado

---
+++ src/main/java/org/generation/lojaDeGames/model/Produto.java
@@ -0,0 +1,79 @@
+package org.generation.lojaDeGames.model;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min = 5, max = 100)
+    private String nome;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String plataforma;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date anoLancamento;
+    
+    @ManyToOne
+    @JsonIgnoreProperties("produtos")
+    private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public Date getAnoLancamento() {
+		return anoLancamento;
+	}
+
+	public void setAnoLancamento(Date anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+}
